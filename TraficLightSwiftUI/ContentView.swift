a3b57c52776b81745fd3fe4073150ed31f7b28c5
polosa00 <i@apolochanin.ru>
2023-06-09 00:17:24 +0300

    Initial Commit

---
+++ TraficLightSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TraficLightSwiftUI
+//
+//  Created by Александр Полочанин on 9.06.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TraficLightSwiftUI/TraficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightSwiftUIApp.swift
+//  TraficLightSwiftUI
+//
+//  Created by Александр Полочанин on 9.06.23.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
