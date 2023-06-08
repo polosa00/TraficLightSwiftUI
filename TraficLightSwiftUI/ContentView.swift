1e5612bb12705f877b04895b7619dc46a251fa5d
polosa00 <i@apolochanin.ru>
2023-06-09 01:40:15 +0300

    Create logic

--- TraficLightSwiftUI/ContentView.swift
+++ TraficLightSwiftUI/ContentView.swift
@@ -8,22 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
-    let
+    enum TraficLight {
+        case red, yellow, green
+    }
+    var buttonTitle = "START"
+    
+    private let lightOn = 1.0
+    private let lightOff = 0.3
+    
+    @State private var currentColor: TraficLight = .red
+    
     var body: some View {
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
             VStack{
-                LightView(color: .red)
-                LightView(color: .yellow)
-                LightView(color: .green)
+                Circle()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(currentColor == .red ? .red : .red.opacity(lightOff))
+                    .overlay(Circle().stroke(Color(.white), lineWidth: 4))
+                    .padding(.top, 10)
+                Circle()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(currentColor == .yellow ? .yellow : .yellow.opacity(lightOff))
+                    .overlay(Circle().stroke(Color(.white), lineWidth: 4))
+                    .padding(.top, 10)
+                Circle()
+                    .frame(width: 100, height: 100)
+                    .foregroundColor(currentColor == .green ? .green : .green.opacity(lightOff))
+                    .overlay(Circle().stroke(Color(.white), lineWidth: 4))
+                    .padding(.top, 10)
+               
                 Spacer()
-                ButtonView()
+                
+                Button(action: buttonPressed) {
+                    
+                    Text("\(buttonTitle)")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                }
+                .frame(width: 150, height: 45)
+                .background(.blue)
+                .cornerRadius(15)
+
+                .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.white, lineWidth: 4))
             }
             .padding()
         }
-      
-        
+    }
+    
+    private func buttonPressed() {
+        switch self.currentColor {
+        case .red:
+            currentColor = .yellow
+        case .yellow:
+           currentColor = .green
+        case .green:
+           currentColor = .red
+        }
     }
 }
 
--- TraficLightSwiftUI/LightView.swift
+++ TraficLightSwiftUI/LightView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct LightView: View {
-    
-    let color: Color
+ 
     var body: some View {
         ZStack {
             Circle()
                 .frame(width: 100, height: 100)
-                .foregroundColor(color)
-            .overlay(Circle().stroke(Color(.white), lineWidth: 4))
+                .foregroundColor(.gray)
+                .overlay(Circle().stroke(Color(.white), lineWidth: 4))
         }
         .padding(.top, 10)
+        .opacity(0.3)
     }
 }
 
 struct LightView_Previews: PreviewProvider {
     static var previews: some View {
-        LightView(color: .red)
+        LightView()
     }
 }
